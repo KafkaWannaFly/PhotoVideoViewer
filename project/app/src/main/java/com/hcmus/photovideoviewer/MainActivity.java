cc58f49f22b0f8b1faff156ed18089d60bd3aa05
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2021-03-09 23:58:32 +0700

    Ask permission, Method for fetching photo and video

--- project/app/src/main/java/com/hcmus/photovideoviewer/MainActivity.java
+++ project/app/src/main/java/com/hcmus/photovideoviewer/MainActivity.java
@@ -1,21 +1,33 @@
 package com.hcmus.photovideoviewer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.hcmus.photovideoviewer.models.PhotoModel;
+import com.hcmus.photovideoviewer.models.VideoModel;
+import com.hcmus.photovideoviewer.services.MediaDataRepository;
 import com.hcmus.photovideoviewer.ui.main.SectionsPagerAdapter;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+	static final int READ_EXTERNAL_CODE = 1;
+	static public boolean EXTERNAL_PERMISSION = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
 		TabLayout tabs = findViewById(R.id.tabs);
 		tabs.setupWithViewPager(viewPager);
 
+		checkPermission();
+
+		ArrayList<PhotoModel> photoModels = MediaDataRepository.getInstance().getPhotoModels();
+		Log.d("Photos", photoModels.toString());
+		ArrayList<VideoModel> videoModels = MediaDataRepository.getInstance().getVideoModels();
+		Log.d("Videos", videoModels.toString());
+
 //		FloatingActionButton fab = findViewById(R.id.fab);
 //		fab.setOnClickListener(new View.OnClickListener() {
 //			@Override
@@ -35,5 +54,32 @@ public class MainActivity extends AppCompatActivity {
 //						.setAction("Action", null).show();
 //			}
 //		});
+	}
+
+	private void checkPermission() {
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+			!= PackageManager.PERMISSION_GRANTED) {
+			requestPermissions(new String[] {
+				Manifest.permission.READ_EXTERNAL_STORAGE
+			}, READ_EXTERNAL_CODE);
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		switch (requestCode) {
+			case READ_EXTERNAL_CODE: {
+				for (int result: grantResults) {
+					if (result != PackageManager.PERMISSION_GRANTED) {
+						EXTERNAL_PERMISSION = false;
+						return;
+					}
+				}
+
+				EXTERNAL_PERMISSION = true;
+			}
+		}
 	}
 }
